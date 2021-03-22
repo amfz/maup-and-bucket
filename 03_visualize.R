e967dd126c7b64014d99be5867f2a739c46778f0
AM <amany.mahfouz@gmail.com>
2021-03-22 15:17:25 -0400

    make more maps

--- 03_visualize.R
+++ 03_visualize.R
@@ -45,17 +45,15 @@ imm_dens_map <-
   tm_shape(full_data) +
   tm_polygons(c("all_imm",
                 "imm_pop",
-                "non_imm_pop",
-                "percent_imm"),
+                "non_pr_pop"),
               convert2density = TRUE,
               area = "sq_km",
               style ="quantile",
-              n = 10,
+              n = 5,
               palette ="-inferno",
               title =c("All Immigrant Population",
                        "Permanent Immigrant Population",
-                       "Non Immigrant Population",
-                       "% Immigrant")) +
+                       "Non-Permanent Immigrant Population")) +
   tm_facets(nrow=2,ncol=2) +
   tm_layout(main.title="Immigration in Toronto, 2016",
             main.title.size =.9,
@@ -64,3 +62,21 @@ imm_dens_map <-
             attr.outside =TRUE)
 
 print(imm_dens_map)
+
+pop_dens_map <-
+  tm_shape(full_data) +
+  tm_polygons(c("imm_dens",
+                "pop_per_sqkm"),
+              style ="quantile",
+              n = 5,
+              palette ="-inferno",
+              title =c("Immigrant Population Density",
+                       "Overall Population Density")) +
+  tm_facets(nrow=2,ncol=1) +
+  tm_layout(main.title="Population Density 2016",
+            main.title.size =.9,
+            legend.text.size=.7,
+            legend.position =c("right","bottom"),
+            attr.outside =TRUE)
+
+print(pop_dens_map)
